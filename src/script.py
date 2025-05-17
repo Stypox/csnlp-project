c36fe43b0b87e60455650e5bdd13a77a03ca3472
Stypox <stypox@pm.me>
2025-05-17 13:27:28 +0200

    Allow passing more than 1 layer

--- src/script.py
+++ src/script.py
@@ -27,12 +27,12 @@ def topleft(a):
 class Traning:
     def __init__(
         self,
-        layer: int,
+        layers: list[int],
         regularization_lambda: float,
         learning_rate: float,
     ):
 
-        self.layer: int = layer
+        self.layers: list[int] = layers
         self.regularization_lambda: float = regularization_lambda
         self.learning_rate: float = learning_rate
         self.loss_fn: nn.CrossEntropyLoss = nn.CrossEntropyLoss()
@@ -49,16 +49,19 @@ class Traning:
 
         self.optimizer: torch.optim.SGD = torch.optim.SGD(self.model.parameters(), lr=learning_rate)
 
+    def is_layer_enabled(self, layer_name: str):
+        return layer_name.startswith(f"model.layers.") and int(layer_name.split(".")[2]) in self.layers
+
     def debug_layer(self):
         for name, param in self.model.named_parameters():
-            if name.startswith(f"model.layers.{self.layer}"):
+            if self.is_layer_enabled(name):
                 print(name, topleft(param.grad))
             else:
                 assert param.grad is None
 
     def enable_only_layer_gradients(self):
         for name, param in self.model.named_parameters():
-            if name.startswith(f"model.layers.{self.layer}"):
+            if self.is_layer_enabled(name):
                 param.requires_grad = True
             else:
                 param.requires_grad = False
@@ -66,7 +69,7 @@ class Traning:
     def get_layer_grad_l2(self):
         res = 0
         for name, param in self.model.named_parameters():
-            if name.startswith(f"model.layers.{self.layer}"):
+            if self.is_layer_enabled(name):
                 res += self.regularization_lambda * torch.norm(param.grad, p=2)
         return res
 
@@ -83,11 +86,11 @@ class Traning:
         loss2.backward()
         self.optimizer.step()
 
-def main(layer: int):
+if __name__ == "__main__":
     t = Traning(
-        layer=layer,
+        layers=[0,1,2,3],
         regularization_lambda=1e-2,
-        learning_rate=1e-4,
+        learning_rate=1e-2,
     )
     while True:
         t.train_on_batch(
@@ -95,6 +98,3 @@ def main(layer: int):
             torch.LongTensor([i for i in range(8)]).to(device),
         )
         t.debug_layer()
-
-if __name__ == "__main__":
-    main(19)
